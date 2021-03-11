706320247deefd9d7f239e4e2878baca729d9c9d
vinnidias <vinicius.3991@gmail.com>
2021-03-11 01:07:55 -0300

    colocando os novos componentes no App Raiz

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import styles from './src/styles'
-import Scanner from   './src/Components/Scanner'
+import Scanner from './src/Components/Scanner'
+import MyQrCode from './src/Components/MyQrCode'
 
 export default function App() {
+  const [visible, setVisible] = useState(false)
+
   return (
-   <Scanner/>
+    <View>
+      {
+        visible
+          ? <Scanner onPress={()=> setVisible(false)}/>
+          : <MyQrCode 
+              onPress={()=> setVisible(true)}
+            />
+      }
+    </View>
+
+
   );
 }
 
