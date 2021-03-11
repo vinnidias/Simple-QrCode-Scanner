5132e793d81b3b1729d1e0a6d80e2e0bdb89b49b
vinnidias <vinicius.3991@gmail.com>
2021-03-11 01:07:13 -0300

    estilos para os componentes

--- src/styles.js
+++ src/styles.js
@@ -1,18 +1,49 @@
 const styles = {
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 
-    headerScannerText: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#001435',
-        alignSelf: 'center',
-        margin: 10
-    }
-  };
+  headerScannerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#001435',
+    alignSelf: 'center',
+    margin: 10
+  },
 
-  export default styles
+  qrCodeContainer: {
+    height: 400,
+    width: 360,
+    justifyContent: 'space-around',
+    alignSelf: 'center',
+    padding: 50,
+    backgroundColor: 'black'
+  },
+
+  buttonContainer: {
+    height: 300,
+    width: 360,
+    justifyContent: 'space-around',
+    alignSelf: 'center',
+    padding: 50,
+  },
+
+  qrButton: {
+    height: 60,
+    width: 250,
+    borderRadius: 5,
+    padding: 20,
+    backgroundColor: 'black'
+  },
+  textButton: {
+    alignSelf: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18
+  }
+};
+
+export default styles
