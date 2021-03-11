76fb847387a0fc68b063a75d2322f432b910d5aa
vinnidias <vinicius.3991@gmail.com>
2021-03-11 01:07:30 -0300

    criação do componente do meu qr code

---
+++ src/Components/MyQrCode.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, Linking, Button, Image, TouchableOpacity } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import DefaultButton from './DefaultButton'
+import styles from '../styles'
+
+export default function MyQrCode({onPress}) {
+    return (
+        <View style={{ flex: 1, padding: 20, marginTop: 50 }}>
+            <View style={styles.qrCodeContainer}>
+                <Image
+                    source={require('../Images/Qr-Code-github.png')}
+                    style={{ height: 330, width: 330, alignSelf: 'center' }}
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <DefaultButton onPress={onPress} title={'Ler um Qr Code!'}/>
+            </View>
+        </View>
+    )
+}
