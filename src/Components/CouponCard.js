df0ba9539100d3d8b17e4ee4c978057ae40d2e3e
vinnidias <vinicius.3991@gmail.com>
2021-04-08 11:10:40 -0300

    correção e ajuste de textos

--- src/Components/CouponCard.js
+++ src/Components/CouponCard.js
@@ -3,15 +3,15 @@ import { Text, View, Alert, Linking, Image, TouchableOpacity } from 'react-nativ
 
 import styles from '../styles'
 
-export default function CouponCard({ imagePath, description, onPress }) {
+export default function CouponCard({ imagePath, description, onPress, codeText }) {
     return (
         <TouchableOpacity style={styles.couponCard} onPress={onPress}>
-            <Image source={imagePath} style={{ width: 200, height: 200, borderRadius: 15, marginBottom: 10}} />
-            <Text style={{fontWeight: 'bold', fontSize: 14, textAlign: 'center'}}>
+            <Image source={imagePath} style={{ width: 180, height: 180, borderRadius: 15, marginBottom: 10}} />
+            <Text style={{fontWeight: 'bold', fontSize: 14, textAlign: 'center', marginBottom: 20}}>
                 {description}
             </Text>
             <Text style={{fontSize: 22, fontWeight: 'bold',  textAlign: 'center'}}>
-                Clique no cupom para acessar o link!
+                {codeText}
             </Text>
 
         </TouchableOpacity>
