61522c688a839c7bfcc1140c50a9c5d18b5d28fd
vinnidias <vinicius.3991@gmail.com>
2021-04-08 11:10:53 -0300

    atualização de novos cupons

--- src/Components/CouponSelector.js
+++ src/Components/CouponSelector.js
@@ -22,26 +22,45 @@ export default function CouponSelector({ route, navigation }) {
             </View>
             <ScrollView>
                 <CouponCard
-                    imagePath={require('../Images/honeygain.png')}
-                    description={'O Honeygain é um serviço que gera uma renda passiva aos usuários que disponibilizam o uso de seu tráfego de rede. O aplicativo é 100% seguro, todas as conexões com os dispositivos são criptografadas com segurança e nenhum dado é armazenado de forma indevida.'}
-                    onPress={() => Linking.openURL('https://r.honeygain.me/VINICE8FD0')}
+                    imagePath={require('../Images/ame-icon.png')}
+                    description={'Ame Digital: Um Super App com cashback e muito mais.Descubra o super app da Ame: pagar, vender com QR code, recarregar o celular, compra créditos para jogar on-line e muito mais.'}
+                    onPress={() => Linking.openURL('https://usea.me/downloadapp')}
+                    codeText={'Use o código vCQiJ na hora de criar sua conta!'}
                 />
-
                 <CouponCard
-                    imagePath={require('../Images/kwai.png')}
-                    description={'O Kwai, famosa rede social de vídeos curtos e tendências para Android e iPhone (iOS), lançou a campanha Kwai Golds para que usuários ganhem dinheiro à medida que completam missões no aplicativo, como assistir a vídeos, fazer check-in diário, convidar pessoas para a plataforma, entre outras atividades.'}
-                    onPress={() => Linking.openURL('https://m.kwai.app/s/WrIRJS98')}
+                    imagePath={require('../Images/picpay-icon.png')}
+                    description={'PicPay. Todo mundo usa, todo lugar aceita. Com PicPay, você movimenta seu dinheiro e faz todos os seus pagamentos de uma maneira simples e rápida. Tudo pelo celular.'}
+                    onPress={() => Linking.openURL('http://www.picpay.com/convite?HW8RJY')}
+                    codeText={'Utilize o código HW8RJY e ganhe R$10,00'}
                 />
 
                 <CouponCard
                     imagePath={require('../Images/meliuz-icon.png')}
                     description={'Descubra como ganhar cashback em suas compras, usando o Méliuz. Dica: você não precisa ter o Cartão Méliuz para ganhar!'}
                     onPress={() => Linking.openURL('https://www.meliuz.com.br/i/ref_fb8f7e0a?ref_source=29')}
+                    codeText={'Toque no cupom para acessar o link!'}
                 />
+
+
+                <CouponCard
+                    imagePath={require('../Images/honeygain.png')}
+                    description={'O Honeygain é um serviço que gera uma renda passiva aos usuários que disponibilizam o uso de seu tráfego de rede. O aplicativo é 100% seguro, todas as conexões com os dispositivos são criptografadas com segurança e nenhum dado é armazenado de forma indevida.'}
+                    onPress={() => Linking.openURL('https://r.honeygain.me/VINICE8FD0')}
+                    codeText={'Toque no cupom para começar com US$ 5,00!'}
+                />
+
+                <CouponCard
+                    imagePath={require('../Images/kwai.png')}
+                    description={'O Kwai, rede social de vídeos curtos e tendências para Android e iPhone, lançou a campanha Kwai Golds para que usuários ganhem dinheiro à medida que completam missões no aplicativo.'}
+                    onPress={() => Linking.openURL('https://m.kwai.app/s/WrIRJS98')}
+                    codeText={'Toque no cupom para acessar o link e ganhar um bônus!'}
+                />
+
                 <CouponCard
                     imagePath={require('../Images/paid-to-read-email.png')}
                     description={'Ganhe dinheiro com o paid-to-read-email.com. Você gostaria de ser pago por ler e-mails pagos online? Parece fácil? Pois é! Receba e-mails pagos diariamente em sua caixa de entrada ...'}
                     onPress={() => Linking.openURL('https://www.paid-to-read-email.com/?r=1070357')}
+                    codeText={'Toque no cupom para acessar o link e já ganhar alguns dólares extra!'}
                 />
 
 
